c07807b95d47c704c3f50fa0d2b314d660997e66
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-04 13:14:31 +0200

    add script

---
+++ script.js
@@ -0,0 +1,5 @@
+const nome = prompt("Inserisci qui il tuo Nome");
+const cognome = prompt("Inserisci qui il tuo cognome");
+const colore = prompt("Inserisci qui il tuo Colore Preferito");
+
+document.writeln("<h1>"+ nome + cognome + colore + "23</h1>");
